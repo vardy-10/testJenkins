2d951d749083a5a9fb1a689158cc39551332e140
zah1996 <835499244@qq.com>
2021-04-08 16:25:52 +0800

    testJenkins

---
+++ demo/src/main/java/com/zah/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.zah.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String test(){
+       return  "ok";
+    }
+}
